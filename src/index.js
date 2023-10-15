d2a888fe371c1a1998c3300efc83239a401e0559
Harvey Rankin <harvey.peter.rankin@outlook.com>
2023-10-15 12:09:58 +0100

    added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { toUpper } from './utils/transformMessage'
 
-const returnMessage = (message) => toUpper(message)
+const returnMessage = (message) =>
+    toUpper(`${message}, this was done in front of a live crowd!`)
 
 export default returnMessage
